6190a189b1bde02f420bdadb7a9e6b5835518d08
Jason Rueckert <phlacin@gmail.com>
2024-07-13 17:15:59 -0700

    bullets kill enemies

--- enemies/enemy_crab.gd
+++ enemies/enemy_crab.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var timer = $Timer
 
+var enemy_death_effect = preload("res://enemies/enemy_death_effect.tscn")
+
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 #const GRAVITY = 1000
 
 @export var speed : int = 1500
 @export var patrol_points : Node
 @export var wait_time : int = 3
+@export var health_amount : int = 3
 
 enum State {
 	IDLE,
@@ -90,3 +93,20 @@ func player_animations():
 		animated_sprite_2d.play("idle")
 	elif current_state == State.WALK:
 		animated_sprite_2d.play("walk")
+
+
+func _on_hurtbox_area_entered(area : Area2D):
+	print("Hurtbox area entered")
+	if area.get_parent().has_method("get_damage_amount"):
+		var node = area.get_parent() as Node
+		health_amount -= node.damage_amount
+		print("Health amount: ", health_amount)
+		
+		if health_amount <= 0:
+			death_effect()
+
+func death_effect():
+	var enemy_death_effect_instance = enemy_death_effect.instantiate() as Node2D
+	enemy_death_effect_instance.global_position = global_position
+	get_parent().add_child(enemy_death_effect_instance)
+	queue_free()
--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var muzzle : Marker2D = $Muzzle
 
-#const GRAVITY = 1000
+var bullet = preload("res://player/bullet.tscn")
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @export var speed : int = 1000
@@ -18,23 +19,29 @@ enum State {
 	IDLE, 
 	RUN, 
 	JUMP,
+	SHOOT,
 	}
 
 var current_state : State
+var muzzle_position
 
 func _ready():
 	current_state = State.IDLE
+	muzzle_position = muzzle.position
 
 func _physics_process(delta : float):
 	player_falling(delta)
 	player_idle(delta)
 	player_run(delta)
 	player_jump(delta)
-	#print("State: ", State.keys()[current_state])
+	player_muzzle_position()
+	
+	player_shooting(delta)
 	
 	move_and_slide()
 	
 	player_animations()
+	#print("State: ", State.keys()[current_state])
 
 func player_falling(delta : float):
 	if !is_on_floor():
@@ -45,7 +52,6 @@ func player_idle(delta):
 		current_state = State.IDLE
 
 func player_run(delta : float):
-	
 	var direction = import_movement()
 	if direction:
 		velocity.x += direction * speed * delta
@@ -67,15 +73,34 @@ func player_jump(delta : float):
 		var direction = import_movement()
 		velocity.x += direction * jump_horizontal_speed * delta
 		velocity.x = clamp(velocity.x, -max_jump_horizontal_speed, max_jump_horizontal_speed)
+
+func player_shooting(delta):
+	var direction = import_movement()
+	
+	if direction != 0 and Input.is_action_just_pressed("shoot"):
+		var bullet_instance = bullet.instantiate() as Node2D
+		bullet_instance.direction = direction
+		bullet_instance.global_position = muzzle.global_position
+		get_parent().add_child(bullet_instance)
+		current_state = State.SHOOT
+
+func player_muzzle_position():
+	var direction = import_movement()
+	if direction > 0:
+		muzzle.position.x = muzzle_position.x
+	elif direction < 0:
+		muzzle.position.x = -muzzle_position.x
 		
 
 func player_animations():
 	if current_state == State.IDLE:
 		animated_sprite_2d.play("idle")
-	elif current_state == State.RUN:
+	elif current_state == State.RUN and animated_sprite_2d.animation != "run_shoot":
 		animated_sprite_2d.play("run")
 	elif current_state == State.JUMP:
 		animated_sprite_2d.play("jump")
+	elif current_state == State.SHOOT:
+		animated_sprite_2d.play("run_shoot")
 
 func import_movement():
 	var direction : float = Input.get_axis("move_left","move_right")
